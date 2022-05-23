cbc342715c203a6f5a133f050a8676e5a611683d
kyj93790 <kyj93790@naver.com>
2022-05-23 19:44:14 +0900

    [yejikim]feat: built-in 명령어 echo 구현

--- src/execute_builtin.c
+++ src/execute_builtin.c
@@ -1,11 +1,48 @@
 #include "head.h"
 #include "execute.h"
 
+int check_n_flag(char *option_str)
+{
+	int	i;
+
+	if (option_str == NULL)
+		return (1);
+	if (option_str[0] != '-')
+		return (1);
+	i = 0;
+	while (option_str[i])
+	{
+		if (option_str[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int execute_echo(t_unit_head *cmd_lst, t_unit_pipe *curr_cmd)
+{
+	int	i;
+	int	n_flag;
+
+	n_flag = check_n_flag(curr_cmd->commands[1]);
+	i = 1;
+	while (curr_cmd->commands[i])
+	{
+		ft_putstr_fd(curr_cmd->commands[i], STDOUT_FILENO);
+		ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+	if (n_flag)
+		ft_putstr_fd('\n', STDOUT_FILENO);
+	return (0);
+}
+
 int	execute_builtin(t_unit_head *cmd_lst, t_unit_pipe *curr_cmd)
 {
 	char	*command;
 
 	command = curr_cmd->commands[0];
+	redirect(curr_cmd->rd);
 	if (ft_strlen(command) == 4 && ft_strncmp(command, "echo", 4) == 0)
 		return (execute_echo(cmd_lst, curr_cmd));
 	if (ft_strlen(command) == 2 && ft_strncmp(command, "cd", 2) == 0)
