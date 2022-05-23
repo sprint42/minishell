bd434a0077c853afc47b26b59ca863d4d8a6e951
Yejin Kim <yejikim@c8r6s10.42seoul.kr>
2022-05-23 22:06:31 +0900

    [yejikim]feat: built-in 명령어 cd 구현

--- src/execute_builtin.c
+++ src/execute_builtin.c
@@ -37,6 +37,52 @@ int execute_echo(t_unit_head *cmd_lst, t_unit_pipe *curr_cmd)
 	return (0);
 }
 
+int	execute_cd_home(void)
+{
+	char	*path;
+
+	path = getenv("$HOME");
+	if (path == NULL)
+	{
+		ft_putstr_fd("HOME not set\n", STDOUT_FILENO);
+		retrurn (1);
+	}
+	if (chdir(path) < 0)
+		exit_with_error();
+	free(path);
+	return (0);
+}
+
+int	execute_cd(t_unit_head *cmd_lst, t_unit_pipe *curr_cmd)
+{
+	char	*pwd;
+	char	*temp;
+	char	*path;
+	
+	if (curr_cmd[1] == NULL)
+		return (execute_cd_home());
+	if (curr_cmd[1][0] != '/')
+	{
+		pwd = getcwd(NULL, 0);
+		if (pwd == NULL)
+			exit_with_error();
+		temp = ft_strjoin(pwd, "/");
+		free(pwd);
+		if (temp == NULL)
+			exit_with_error();
+		path = ft_strjoin(temp, curr_cmd[1]);
+		free(temp);
+		if (path == NULL)
+			exit_with_error();
+	}
+	else
+		path = curr_cmd[1];
+	if (chdir(path) < 0)
+		exit_with_error();
+	free(path);
+	return (0);
+}
+
 int	execute_builtin(t_unit_head *cmd_lst, t_unit_pipe *curr_cmd)
 {
 	char	*command;
