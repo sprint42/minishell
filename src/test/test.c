4090da6f9619ccacc846757a7b405bf2c810a530
charmingjae <beingeskimo@gmail.com>
2022-05-26 14:07:12 +0900

    [mcha]refact: 주석 추가적으로 삭제

--- src/test/test.c
+++ src/test/test.c
@@ -6,21 +6,10 @@ void    test(void)
 	char	*ret;
 	char	*tmp_target;
 	t_point	*pck;
-	// (void)target;
-	// (void)ret;
-	// (void)tmp_target;
 
 	pck = init_struct();
 	init_special(pck);
 	init_environ(pck);
-	// t_unit_env	*a;
-
-	// a = pck->env->env_next;
-	// while (a->env_next)
-	// {
-	// 	printf("%s, %s\n", a->key, a->value);
-	// 	a = a->env_next;
-	// }
 	target = "             one		two args | three| <<  four>	\"	\" \" \" five>>     	$P\'A\'TH\"$PATH \'abcd\'\"	$PATH\'hello\'world previous quote\'testsquote\' six| asdfasdfasdf\"$PATH$USER it>>is String\' hello trillion world  \'a\"b";
 	// target = "   	\'e\'ch\"o\" 	 a      cc >> hello| <<wtf|   b	USER\'$PATH\'wtf\"$HELLO\" 	";
 	// // target="hello \"world\"ab|c\'after that\'";
@@ -33,20 +22,15 @@ void    test(void)
 	tmp_target = target;
 	while (*tmp_target)
 	{
-		// printf("now ret : %s\n", ret);
-		// printf("%s, %s\n", pck->env->env_next->key, pck->env->env_next->value);
 		judge_special(pck, &tmp_target, &ret);
 	}
-	// t_unit_env	*ptr;
-
-	// ptr = pck->env;
-	// while (ptr->env_next)
-	// {
-	// 	printf("%s, %s\n", ptr->env_next->key, ptr->env_next->value);
-	// 	ptr = ptr->env_next;
-	// }
-	// free(target);
 	manufact(pck);
+	/*
+	**	type 가공이 끝난 후 t_unit_head에 파이프 혹은 리다이렉션 단위로 끊어서 연결
+	*/
+	/*
+		
+	*/
 	free_package(pck);
 	free(target);
 }
