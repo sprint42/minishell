e842afcbc5a5bea0fba653cd0aa3c6b046459aa7
kyj93790 <kyj93790@naver.com>
2022-05-27 00:40:37 +0900

    [yejikim]feat: execve를 위한 envp, file 경로 생성 구현

--- src/execute_child.c
+++ src/execute_child.c
@@ -1,15 +1,73 @@
 #include "head.h"
 #include "execute.h"
 
+char	**extract_path(char **envp, t_unit_pipe *curr_cmd)
+{
+	char	**path;
+	int		i;
+
+	if (!envp)
+		handle_child_process_error(1, errno, curr_cmd->commands[0]);
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			break ;
+		i++;
+	}
+	if (envp[i] == NULL)
+		handle_child_process_error(1, errno, curr_cmd->commands[0]);
+	path = ft_split(&envp[i][5], ':');
+	if (path == NULL)
+		handle_child_process_error(1, errno, curr_cmd->commands[0]);
+	return (path);
+}
+
+void	add_path(t_unit_pipe *curr_cmd, char **path)
+{
+	char	*temp_path;
+	char	*temp_cmd;
+	int		i;
+
+	i = 0;
+	while (path[i])
+	{
+		temp_path = ft_strjoin(path[i], "/");
+		if (temp_path == NULL)
+			handle_child_process_error(1, errno, curr_cmd->commands[0]);
+		temp_cmd = ft_strjoin(temp_path, curr_cmd->commands[0]);
+		free(temp_path);
+		if (temp_cmd == NULL)
+			handle_child_process_error(1, errno, curr_cmd->commands[0]);
+		if (access(temp_cmd, X_OK) < 0)
+			free(temp_cmd);
+		else
+		{
+			free(curr_cmd->commands[0]);
+			curr_cmd->commands[0] = temp_cmd;
+			return ;
+		}
+		i++;
+	}
+}
+
 int	execute_execve(t_unit_head *cmd_lst, t_unit_pipe *curr_cmd, int **pipe_fd, int i)
 {
 	char	**envp;
+	char	**path;
 
 	envp = make_env_array(cmd_lst);
 	if (envp == NULL)
 		return (handle_child_process_error(1, errno, curr_cmd->commands[0]));
-	// 환경변수 parsing 하고 호출, exit_status 처리
-	// execve(curr_cmd->commands[0], curr_cmd->command, envp)
+	if (ft_strchr(curr_cmd, '/') == NULL)
+	{
+		path = extract_path(envp, curr_cmd);
+		if (path != NULL)
+			add_path(curr_cmd, path);
+		free_path(path);
+	} 
+	execve(curr_cmd->commands[0], curr_cmd->commands, envp);
+	// exit_status
 }
 
 t_unit_pipe	*find_curr_cmd(t_unit_head *cmd_lst, int i)
