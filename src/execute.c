d76053217537e4d130ef6b044974081e1751d704
kyj93790 <kyj93790@naver.com>
2022-05-29 23:51:31 +0900

    [yejikim]feat: child process exit status 계산 수정

--- src/execute.c
+++ src/execute.c
@@ -11,7 +11,7 @@ int	wait_childs(t_unit_head *cmd_lst)
 			waitpid(cmd_lst->child.pid[i], &(cmd_lst->child.status[i]), 0);
 		i++;
 	}
-	g_exit_status = (cmd_lst->child.status[--i] & 0x0f) >> 8;
+	g_exit_status = (cmd_lst->child.status[--i] >> 8);
 	free_cmd_lst(cmd_lst);
 	return (0);
 }
